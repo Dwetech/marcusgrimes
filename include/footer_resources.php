30cf4e3e5411dbe4364a2b5b41e0e2f401a94240
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2014-09-24 15:19:04 +0600

    header fixed

--- include/footer_resources.php
+++ include/footer_resources.php
@@ -2,3 +2,4 @@
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
